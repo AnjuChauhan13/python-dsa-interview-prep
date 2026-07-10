d2a1f12d0d376311fbecd4fed62cf014ed6877d3
AnjuChauhan13 <anju34111@gmail.com>
2026-07-10 12:01:03 +0530

    merged sort

---
+++ Sorting Algo/Merge_sort.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    merged = []
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+
+arr = [8, 3, 5, 4, 7, 6, 1, 2]
+
+print(merge_sort(arr))
